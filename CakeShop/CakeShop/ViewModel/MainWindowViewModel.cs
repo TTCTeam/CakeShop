ad35e98eddd6f0e8e262c7c9bef32c200de90bf6
Hà Minh Cường <55825733+haminhcuong2k@users.noreply.github.com>
2020-12-28 15:34:07 +0700

    ControllBar

--- CakeShop/CakeShop/ViewModel/MainWindowViewModel.cs
+++ CakeShop/CakeShop/ViewModel/MainWindowViewModel.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace CakeShop.ViewModel
@@ -13,27 +14,42 @@ namespace CakeShop.ViewModel
         public event PropertyChangedEventHandler PropertyChanged;
         public int SwitchView { get; set; } = 0;
 
-        public ICommand MinimizeCommand { get; set; }
-
         private int _SelectedIndex;
-
         public int SelectedIndex
         {
             get { return _SelectedIndex; }
             set { _SelectedIndex = value; SwitchView = SelectedIndex; }
         }
 
-
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MaximizeWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommand { get; set; }
         public MainWindowViewModel()
         {
-            MinimizeCommand = new RelayCommand<object>((p) =>
+            CloseWindowCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
             {
-                return true;
-            },
-                (p) =>
+                var mess = MessageBox.Show("Do you want to exit ?", "Notification", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No);
+                if (mess == MessageBoxResult.Yes)
                 {
-                    
-                });
+                    p.Close();
+                }
+            }
+            );
+
+            MaximizeWindowCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                if (p.WindowState != WindowState.Maximized)
+                    p.WindowState = WindowState.Maximized;
+                else
+                    p.WindowState = WindowState.Normal;
+            }
+            );
+
+            MinimizeWindowCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                p.WindowState = WindowState.Minimized;
+            }
+            );
         }
     }
 }
